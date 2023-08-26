284f1dc5aab2b44bb013023decf4e12b9aef3c5f
Miguel Clement <miguelclement@gmail.com>
2023-08-26 18:29:27 -0400

    67. Add Binary

---
+++ add_binary.py
@@ -0,0 +1,33 @@
+def addBinary(self, a: str, b: str) -> str:
+    # Initialize carry to 0
+    carry = 0
+    
+    # Initialize index to the last element of digits
+    i = len(a) - 1
+    j = len(b) - 1
+    
+    # Initialize result to empty string
+    result = ''
+    
+    while i >= 0 or j >= 0:
+        # Add carry to current digits[index]
+        if i >= 0:
+            carry += int(a[i])
+        if j >= 0:
+            carry += int(b[j])
+        
+        # Append the result of carry % 2 to the result string
+        result = str(carry % 2) + result
+        
+        # Update the carry for the next iteration
+        carry //= 2
+
+        # Decrement the indices
+        i -= 1
+        j -= 1
+    
+    # If carry is still 1 after the loop, prepend 1 to the beginning of the result
+    if carry == 1:
+        result = '1' + result
+    
+    return result
